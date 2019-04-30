f0563b6d3ad06811642b315d02ddfdc0576611cf
Allan Cruz <allancruzrosa@gmail.com>
2019-04-30 12:01:11 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/allan/cursomc/resources/ClienteResouce.java
+++ src/main/java/com/allan/cursomc/resources/ClienteResouce.java
@@ -1,5 +1,6 @@
 package com.allan.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.allan.cursomc.domain.Cliente;
 import com.allan.cursomc.dto.ClienteDTO;
+import com.allan.cursomc.dto.ClienteNewDTO;
 import com.allan.cursomc.services.ClienteService;
 
 @RestController
@@ -34,6 +37,15 @@ public class ClienteResouce {
 		
 		
 	
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto ){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- src/main/java/com/allan/cursomc/services/ClienteService.java
+++ src/main/java/com/allan/cursomc/services/ClienteService.java
@@ -9,10 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.allan.cursomc.domain.Cidade;
 import com.allan.cursomc.domain.Cliente;
+import com.allan.cursomc.domain.Endereco;
+import com.allan.cursomc.domain.enums.TipoCliente;
 import com.allan.cursomc.dto.ClienteDTO;
+import com.allan.cursomc.dto.ClienteNewDTO;
+import com.allan.cursomc.repositories.CidadeRepository;
 import com.allan.cursomc.repositories.ClienteRepository;
+import com.allan.cursomc.repositories.EnderecoRepository;
 import com.allan.cursomc.services.exceptions.DataIntegrityException;
 import com.allan.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,11 +29,25 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -63,11 +84,38 @@ public class ClienteService {
 		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
 	}
 	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli =  new Cliente(null,objDto.getNome(),objDto.getEmail(),objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()));
+		
+		Cidade cid = findById(objDto.getCidadeId());
+		
+		Endereco end = new Endereco(null, objDto.getLogadouro(),objDto.getNumero(),objDto.getComplemento(),objDto.getBairro(),objDto.getCep(),cli,cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
+		
+		
+	}
+	
+	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 		
 	}
 	
+	
+	private Cidade findById(Integer id) {
+		Optional<Cidade> obj = cidadeRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cidade.class.getName()));
+		}
 
 }
