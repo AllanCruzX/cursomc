ab564231dd50340c5d3fd9adb5059900908f1cc6
Allan Cruz <allancruzrosa@gmail.com>
2019-07-24 10:01:50 -0300

    Enviando imagem via endpoint

--- src/main/java/com/allan/cursomc/services/ClienteService.java
+++ src/main/java/com/allan/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.allan.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.allan.cursomc.domain.Cidade;
 import com.allan.cursomc.domain.Cliente;
@@ -41,6 +43,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3service;
 	
 	
 	
@@ -133,5 +138,10 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cidade.class.getName()));
 		}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3service.uploadFile(multipartFile);
+	}
+	
 
 }
