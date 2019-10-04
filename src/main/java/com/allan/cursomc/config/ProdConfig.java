0458ab7988692e96caad6af6bf67a4acc72f85d8
allanCruzX <allancruzrosa@gmail.com>
2019-10-03 22:33:03 -0300

    envio para producao com ajustes

--- src/main/java/com/allan/cursomc/config/ProdConfig.java
+++ src/main/java/com/allan/cursomc/config/ProdConfig.java
@@ -13,8 +13,8 @@ import com.allan.cursomc.services.EmailService;
 import com.allan.cursomc.services.SmtpEmailService;
 
 @Configuration
-@Profile("dev")
-public class DevConfig {
+@Profile("prod")
+public class ProdConfig {
 	
 	@Autowired
 	private DBService dbService;
